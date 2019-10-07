ad1e3d17b840c79e884c2b988a8cd20d58c1a775
VanGabs <48846169+VanGabs@users.noreply.github.com>
2019-10-06 22:14:19 -0300

    Add files via upload

---
+++ Validar_cpf/index.php
@@ -0,0 +1,46 @@
+
+<html lang="pt-br">
+    <title> Validador de CPF </title>
+        <head>
+                <meta charset="utf-8">
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <link rel="stylesheet" href="/CSS/styles.css">
+                
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        
+        </head>
+       
+        <header>
+                <label id="textoheader" > Validador de CPF </label>
+                  
+        </header>
+                
+        <div class="container"> 
+                       
+        <body>      
+                     
+                <div class="texto">
+                                
+                        <form action="validarcpf.php" method="POST">
+                                
+                                <div class="form-group">
+                                        <label for="cpf" id="cortexto">CPF:</label>
+                                        <input type="text" maxlength="9" name="cpf" placeholder="Seu cpf">
+                                        <input type="text" maxlength="2" name="dig" size="5" placeholder="Digito">
+                                </div>              
+                                
+                                        <input type="submit" class="btn btn-dark" name="enviar" >                        
+                        </form>
+        
+                                                               
+                        
+                </div>
+        </body>
+</html>
+        
+                                                     
+                                                    
+                                        
+                               
+                
